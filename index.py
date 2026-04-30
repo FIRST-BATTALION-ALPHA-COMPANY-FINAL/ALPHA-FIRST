a286ebed160296a0b654880ff688494837206aa0
AlinorSangcopan <sangcopan.alinor321@gmail.com>
2026-04-30 00:43:19 +0000

    PLATOON LEADER

--- index.py
+++ index.py
@@ -40,6 +40,26 @@ while True:
         except FileNotFoundError:
             print("FILE NOT FOUND!")
 
+    elif option == "3":
+    word = input("ENTER WORD TO SEARCH: ")
+
+        try:
+            with open("diary.txt", "r") as file:
+                lines = file.readlines()
+
+            found = 0
+
+            for line in lines:
+                if word in line:
+                    print("FOUND:", line.strip())
+                    found = found + 1
+
+            if found == 0:
+                print("NO MATCH FOUND!")
+
+        except FileNotFoundError:
+            print("FILE NOT FOUND!")
+
 
     elif option == "5":
         print("DISMISSED!")
