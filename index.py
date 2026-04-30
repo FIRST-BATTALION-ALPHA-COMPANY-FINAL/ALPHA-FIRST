0393bc8470d31ab9b14488054e78d64285a3ea7c
AlinorSangcopan <sangcopan.alinor321@gmail.com>
2026-04-30 08:39:55 +0800

    PLATOON LEADER

--- index.py
+++ index.py
@@ -40,56 +40,6 @@ while True:
         except FileNotFoundError:
             print("FILE NOT FOUND!")
 
-    elif option == "3":
-    wordSearch = input("ENTER WORD TO SEARCH: ")
-
-        try:
-            with open("diary.txt", "r") as file:
-                lines = file.readlines()
-    
-            found = 0
-    
-            for line in lines:
-                if word in line:
-                    print("FOUND:", line.strip())
-                    found = found + 1
-    
-            if found == 0:
-                print("NO MATCH FOUND!")
-    
-        except FileNotFoundError:
-            print("FILE NOT FOUND!")
-
-    elif option == "4":
-        try:
-        with open("diary.txt", "r") as file:
-            lines = file.readlines()
-
-        print("\n--- DIARY CONTENT ---")
-
-        number = 1
-        for line in lines:
-            print(number, "-", line.strip())
-            number = number + 1
-
-        choice = int(input("ENTER NUMBER TO DELETE: "))
-
-        if choice >= 1 and choice <= len(lines):
-            del lines[choice - 1]
-
-            with open("diary.txt", "w") as file:
-                for line in lines:
-                    file.write(line)
-
-            print("ENTRY ELIMINATED!")
-        else:
-            print("INVALID NUMBER!")
-
-    except FileNotFoundError:
-        print("FILE NOT FOUND!")
-
-    except ValueError:
-        print("ENTER NUMBERS ONLY!")
 
     elif option == "5":
         print("DISMISSED!")
